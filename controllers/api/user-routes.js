ee543b34a255259d096eab62e032cfd51448a7fe
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-18 22:05:13 -0400

    user-routes.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// ===============
-// CRUD OPERATIONS
-// ===============
+// GET all users
+// POST single user
+// LOGIN
+// LOGOUT
+
+// GET /api/users for testing purposes
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 // CREATE api/users
 router.post('/', async (req, res) => {
@@ -28,6 +41,7 @@ router.post('/', async (req, res) => {
 // Login Route for Authentication - compares user-entered password to hashed password
 router.post('/login', async (req, res) => {
   try {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
     const user = await User.findOne({
       where: {
         username: req.body.username,
@@ -35,29 +49,33 @@ router.post('/login', async (req, res) => {
     });
 
     if (!user) {
-      res.status(400).json({ message: 'No user account found!' });
+      // does the user exist?
+      res.status(400).json({ message: 'No user account found.' });
       return;
     }
 
+    // verify user using the password they entered in the body
     const validPassword = user.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'No user account found!' });
+      res.status(400).json({ message: 'No user account found.' });
       return;
     }
 
+    // save session information
     req.session.save(() => {
       req.session.userId = user.id;
       req.session.username = user.username;
       req.session.loggedIn = true;
 
-      res.json({ user, message: 'You are now logged in!' });
+      res.json({ user, message: 'You are now logged in.' });
     });
   } catch (err) {
-    res.status(400).json({ message: 'No user account found!' });
+    res.status(400).json({ message: 'No user account found.' });
   }
 });
 
+// LOGOUT by destroying session variables and resetting cookie
 router.post('/logout', (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
